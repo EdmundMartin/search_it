d588e8b90fab7ee6c86b84eca15d06cd423dcd2f
Francois Liot <francois.liot@cgm.com>
2023-02-04 23:00:18 +0100

    remove yandex & publish qwant

--- searchit/__init__.py
+++ searchit/__init__.py
@@ -1,12 +1,12 @@
-from searchit.scrapers.google import GoogleScraper
-from searchit.scrapers.yandex import YandexScraper
-from searchit.scrapers.bing import BingScraper
 from searchit.scrapers.scraper import ScrapeRequest
+from searchit.scrapers.bing import BingScraper
+from searchit.scrapers.google import GoogleScraper
+from searchit.scrapers.qwant import QwantScraper
 
 
 __all__ = [
-    "GoogleScraper",
-    "YandexScraper",
-    "BingScraper",
     "ScrapeRequest",
+    "BingScraper",
+    "GoogleScraper",
+    "QwantScraper",
 ]
